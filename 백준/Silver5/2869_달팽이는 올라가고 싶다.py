2b63b23b56f7419d4156eab606be26b2d0f6ca27
Glory <dudrhkd6550@naver.com>
2022-08-14 20:30:21 +0900

    220814-달팽이코드 수정

--- 백준/Silver5/2869_달팽이는 올라가고 싶다.py
+++ 백준/Silver5/2869_달팽이는 올라가고 싶다.py
@@ -1,16 +1,8 @@
-# A,B,V = map(int,input().split())
-# day = 0
-# while True:
-#     if A >= V:
-#         day +=1
-#         print(day)
-#         break
-#     V -= (A-B)
-#     day += 1
-A,B,V = map(int, input().split())
+A,B,V = map(int,input().split())
 
-if (V-B) % (A-B) == 0 :
-    print((V-B) // (A-B))
-else :
-    print(((V-B) // (A-B)) +1)
+if ((V-B)%(A-B)==0):
+    print((V-B)//(A-B))
+else:
+    print(((V - B) // (A - B))+1)
+
 
